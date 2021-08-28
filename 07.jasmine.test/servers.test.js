0fe4d7bf5afd596617a3c9f26315190e30070e44
BreadsticksN7 <86450828+BreadsticksN7@users.noreply.github.com>
2021-08-27 20:42:50 -0600

    Add files via upload

---
+++ 07.jasmine.test/servers.test.js
@@ -0,0 +1,37 @@
+describe("Servers test (with setup and tear-down)", function() {
+  beforeEach(function () {
+    // initialization logic
+    serverNameInput.value = 'Alice';
+  });
+
+  it('should add a new server to allServers on submitServerInfo()', function () {
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(1);
+    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+  });
+
+  it('should not add empty inputs', function(){
+    serverNameInput.value = '';
+    submitServerInfo();
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+  it('should updateServerTable()', function(){
+    submitServerInfo();
+    updateServerTable();
+    let curTdList = document.querySelectorAll('#serverTable tbody tr td');
+
+    expect(curTdList.length).toEqual(3);
+    expect(curTdList[0].innerText).toEqual('Alice');
+    expect(curTdList[1].innerText).toEqual('$0.00');
+    expect(curTdList[2].innerText).toEqual('X');
+  });
+
+  afterEach(function() {
+    // teardown logic
+    serverId = 0;
+    serverTbody.innerHTML = '';
+    allServers = {};
+  });
+});
