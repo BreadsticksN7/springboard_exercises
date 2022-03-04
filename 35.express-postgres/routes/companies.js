ad9b1cbf54fe74aa359f964c8a7f3533849a0b03
BreadsticksN7 <86450828+BreadsticksN7@users.noreply.github.com>
2022-03-03 20:54:47 -0700

    query changes(still fail)

--- 35.express-postgres/routes/companies.js
+++ 35.express-postgres/routes/companies.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const ExpressError = require('../expressError');
-const router = express.Router();
+const router = new express.Router();
 const db = require('../db');
 
 router.get('/', async (req, res, next) => {
   try {
-    const results = await db.query(`SELECT * FROM companies`);
+    const results = await db.query(`SELECT name FROM companies`);
     return res.json({ companies: results.row })
   } catch(e) {
     return next(e);
