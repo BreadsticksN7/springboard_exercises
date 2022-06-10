608de4562539f5571321ad5d1db467f46213465a
Breadsticks <bread@BreadsMac.local>
2022-06-10 11:47:40 -0600

    react-jobly final(no tests)

---
+++ 43.react-jobly/frontend/build/precache-manifest.70ef29df810d31af0c5e4a9c23d4b3c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b8c7df335be0d778e19",
+    "url": "/static/css/main.51da7c98.chunk.css"
+  },
+  {
+    "revision": "4b8c7df335be0d778e19",
+    "url": "/static/js/main.4b8c7df3.chunk.js"
+  },
+  {
+    "revision": "43570e789ca3017f35c0",
+    "url": "/static/css/1.049ba62b.chunk.css"
+  },
+  {
+    "revision": "43570e789ca3017f35c0",
+    "url": "/static/js/1.43570e78.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "837c8543fb0fbc17ac157848800f2553",
+    "url": "/index.html"
+  }
+];
